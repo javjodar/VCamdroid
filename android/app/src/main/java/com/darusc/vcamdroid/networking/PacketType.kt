c7280e2133dd70f8dc4f75019832efd3ebb30a5b
hypertensiune <dariuscirneala05@gmail.com>
2026-01-01 16:52:18 +0200

    Added untracked android files

---
+++ android/app/src/main/java/com/darusc/vcamdroid/networking/PacketType.kt
@@ -0,0 +1,13 @@
+package com.darusc.vcamdroid.networking
+
+class PacketType {
+    companion object {
+        const val FRAME: Byte = 0x00
+        const val RESOLUTION: Byte = 0x01
+        const val ACTIVATION: Byte = 0x02
+        const val CAMERA: Byte = 0x03
+        const val QUALITY: Byte = 0x04
+        const val WB: Byte = 0x05
+        const val EFFECT: Byte = 0x06
+    }
+}
---
+++ android/app/src/main/java/com/darusc/vcamdroid/rtsp/StreamOptions.kt
@@ -0,0 +1,17 @@
+package com.darusc.vcamdroid.rtsp
+
+import com.pedro.encoder.input.video.CameraHelper
+
+data class StreamOptions(
+    var camera: CameraHelper.Facing,
+    var bitrate: Int,
+    var width: Int,
+    var height: Int,
+    var fps: Int,
+) {
+    companion object {
+        fun default(): StreamOptions {
+            return StreamOptions(CameraHelper.Facing.BACK, 4000 * 1024, 640, 480, 30)
+        }
+    }
+}
